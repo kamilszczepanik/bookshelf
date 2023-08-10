e7afeda0c288a62c2f5907354646a942e97fc502
kamilszczepanik <kszczepanikworkspace@gmail.com>
2023-08-10 16:46:59 +0200

    [feature] Logo with buttons rendered

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,24 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+const {Logo} = require('components/logo')
+const {createRoot} = require('react-dom/client')
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+const App = () => {
+  const handleLogin = () => {
+    console.log('clicked handle login')
+  }
+  const handleRegister = () => {
+    console.log('clicked handle register')
+  }
+  return (
+    <div>
+      <Logo />
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  )
+}
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+export {root}
