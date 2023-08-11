39beb012c90bdb5ea8d2fe093da20b9dfbb6d6d3
kamilszczepanik <kszczepanikworkspace@gmail.com>
2023-08-11 18:43:20 +0200

    [feature] Router for discover and books

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -2,14 +2,13 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// We'll be doing a lot of stuff with the router on this page.
-// 🐨 Here's what you'll need to import from react-router-dom
-// Routes, Route, Link
+import {Routes, Route, Link} from 'react-router-dom'
 import {Button} from './components/lib'
 import * as mq from './styles/media-queries'
 import * as colors from './styles/colors'
-// 🐨 you'll need to import all the screen components in the screens directory
-// 💰 DiscoverBooksScreen, BookScreen, NotFoundScreen
+import {NotFoundScreen} from 'screens/not-found.exercise'
+import {BookScreen} from 'screens/book.exercise'
+import {DiscoverBooksScreen} from 'screens/discover'
 
 function AuthenticatedApp({user, logout}) {
   return (
@@ -56,9 +55,9 @@ function AuthenticatedApp({user, logout}) {
 }
 
 function NavLink(props) {
-  // 🐨 change this from an <a /> to a <Link />
   return (
-    <a
+    <Link
+      to={'/discover'}
       css={{
         display: 'block',
         padding: '8px 15px 8px 10px',
@@ -101,11 +100,7 @@ function Nav() {
         }}
       >
         <li>
-          {/*
-              🐨 Once the NavLink has been updated to use a Router Link,
-                change from the href prop to a "to" prop
-          */}
-          <NavLink href="/discover">Discover</NavLink>
+          <NavLink to="/discover">Discover</NavLink>
         </li>
       </ul>
     </nav>
@@ -113,14 +108,13 @@ function Nav() {
 }
 
 function AppRoutes({user}) {
-  // 🐨 Return all the routes here.
-  // 💰 Here's the mapping of URL to element:
-  //     /discover         <DiscoverBooksScreen user={user} />
-  //     /book/:bookId     <BookScreen user={user} />
-  //     *                 <NotFoundScreen />
-  //
-  // Make sure to check the INSTRUCTIONS.md for how this should be structured
-  return null
+  return (
+    <Routes>
+      <Route path="/discover" element={<DiscoverBooksScreen user={user} />} />
+      <Route path="/book/:bookId" element={<BookScreen user={user} />} />
+      <Route path="*" element={<NotFoundScreen />} />
+    </Routes>
+  )
 }
 
 export {AuthenticatedApp}
