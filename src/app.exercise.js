fc184d48b95da8f5ee4115e3d45544a7e67404bc
kamilszczepanik <kszczepanikworkspace@gmail.com>
2023-08-11 18:17:42 +0200

    [feature] Auth

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,24 +2,70 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
-// import * as auth from 'auth-provider'
+import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from 'utils/api-client.exercise'
+import {useAsync} from 'utils/hooks'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from './styles/colors'
+
+const getUser = async () => {
+  let user = null
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
-  // 🐨 useState for the user
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync()
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
+  React.useEffect(() => {
+    run(getUser().then(u => setUser(u)))
+  }, [run, setUser])
 
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
+  const login = form => auth.login(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setUser(u))
 
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
+  const logout = () => {
+    auth.logout()
+    setUser(null)
+  }
 
-  return <UnauthenticatedApp />
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : isLoading ? (
+    <FullPageSpinner />
+  ) : isError ? (
+    <div
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
 export {App}
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,12 +1,30 @@
-const apiURL = process.env.REACT_APP_API_URL
+import * as auth from 'auth-provider'
 
-function client(endpoint, customConfig = {}) {
+const apiURL = process.env.REACT_APP_API_URL.catch(error => {
+  if (error.status === 401) {
+    auth.logout()
+    window.location.assign(window.location)
+  }
+})
+function client(
+  endpoint,
+  {token, headers: customHeaders, ...customConfig} = {},
+) {
   const config = {
     method: 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+      ...customHeaders,
+    },
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      await auth.logout()
+      window.location.assign(window.location)
+    }
+
     const data = await response.json()
     if (response.ok) {
       return data
